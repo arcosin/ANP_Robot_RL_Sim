fa76f3c6a1d18de231d1017d549abb003f1f7f6d
Haozhe Zhou <43046309+haozheee@users.noreply.github.com>
2021-03-31 07:11:34 -0400

    Create sac_two_outputs.py

---
+++ sac_two_outputs.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
+
+from model import ValueNetwork, SoftQNetwork, PolicyNetwork
+from common.replay_buffers import BasicBuffer
+import numpy as np
+
+
+class SACAgent:
+
+    def __init__(self, env, gamma, tau, v_lr, q_lr, policy_lr, buffer_maxlen):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.env = env
+        # self.action_range = [env.action_space.low, env.action_space.high]
+        # TODO: as a simple demo, I changed here; for the implementation, we should pass this as parameters
+        self.action_range = [[-1, 1], [-1, 1]]
+        self.obs_dim = env.observation_space.shape[0]
+        self.action_dim = 2
+        # self.action_dim = 1
+
+        # hyperparameters
+        self.gamma = gamma
+        self.tau = tau
+        self.update_step = 0
+        self.delay_step = 2
+
+        # initialize networks
+        self.value_net = ValueNetwork(self.obs_dim, 1).to(self.device)
+        self.target_value_net = ValueNetwork(self.obs_dim, 1).to(self.device)
+        self.q_net1 = SoftQNetwork(self.obs_dim, self.action_dim).to(self.device)
+        self.q_net2 = SoftQNetwork(self.obs_dim, self.action_dim).to(self.device)
+        self.policy_net = PolicyNetwork(self.obs_dim, self.action_dim).to(self.device)
+
+        # copy params to target param
+        for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
+            target_param.data.copy_(param)
+
+        # initialize optimizers
+        self.value_optimizer = optim.Adam(self.value_net.parameters(), lr=v_lr)
+        self.q1_optimizer = optim.Adam(self.q_net1.parameters(), lr=q_lr)
+        self.q2_optimizer = optim.Adam(self.q_net2.parameters(), lr=q_lr)
+        self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=policy_lr)
+
+        self.replay_buffer = BasicBuffer(buffer_maxlen)
+
+    # pi: state -> acton
+    def get_action(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+        mean, log_std = self.policy_net.forward(state)
+        std = log_std.exp()
+
+        normal = Normal(mean, std)
+        z = normal.sample()
+        action = torch.tanh(z)
+        action = action.cpu().detach().squeeze(0).numpy()
+
+        return self.rescale_action(action)
+
+    def rescale_action(self, action):
+        '''if action < 0.5:
+            return 0
+        else:
+            return 1'''
+        scaled_action = []
+        for idx, a in enumerate(action):
+            action_range = self.action_range[idx]
+            a = (action_range[1] - action_range[0]) / 2.0 + (action_range[1] + action_range[0]) / 2.0
+            scaled_action.append(a)
+        return scaled_action
+
+    def update(self, batch_size):
+        states, actions, rewards, next_states, dones = self.replay_buffer.sample(batch_size)
+        states = torch.FloatTensor(states).to(self.device)
+        actions = torch.FloatTensor(actions).to(self.device)
+        rewards = torch.FloatTensor(rewards).to(self.device)
+        next_states = torch.FloatTensor(next_states).to(self.device)
+        dones = torch.FloatTensor(dones).to(self.device)
+        dones = dones.view(dones.size(0), -1)
+
+        next_actions, next_log_pi = self.policy_net.sample(next_states)
+        next_q1 = self.q_net1(next_states, next_actions)
+        next_q2 = self.q_net2(next_states, next_actions)
+        next_v = self.target_value_net(next_states)
+
+        # value Loss
+        next_v_target = torch.min(next_q1, next_q2) - next_log_pi
+        curr_v = self.value_net.forward(states)
+        v_loss = F.mse_loss(curr_v, next_v_target.detach())
+
+        #TODO: Question: why using 2 Q-networks?
+        # To reduce bias in training.
+
+        # q loss
+        curr_q1 = self.q_net1.forward(states, actions)
+        curr_q2 = self.q_net2.forward(states, actions)
+        expected_q = rewards + (1 - dones) * self.gamma * next_v
+        q1_loss = F.mse_loss(curr_q1, expected_q.detach())
+        q2_loss = F.mse_loss(curr_q2, expected_q.detach())
+
+        # update value network and q networks
+        self.value_optimizer.zero_grad()
+        v_loss.backward()
+        self.value_optimizer.step()
+
+        self.q1_optimizer.zero_grad()
+        q1_loss.backward()
+        self.q1_optimizer.step()
+
+        self.q2_optimizer.zero_grad()
+        q2_loss.backward()
+        self.q2_optimizer.step()
+
+        # delayed update for policy net and target value nets
+        # TODO: Question: what does this part do?
+        # The original paper mentioned 2 methods for approximating the value function
+        # 1. the EMA of policy weights to update the Q network
+        # 2. periodical update of the policy network, which is used in this code
+        if self.update_step % self.delay_step == 0:
+            new_actions, log_pi = self.policy_net.sample(states)
+            min_q = torch.min(
+                self.q_net1.forward(states, new_actions),
+                self.q_net2.forward(states, new_actions)
+            )
+            policy_loss = (log_pi - min_q).mean()
+
+            self.policy_optimizer.zero_grad()
+            policy_loss.backward()
+            self.policy_optimizer.step()
+
+            # target networks
+            for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
+                target_param.data.copy_(self.tau * param + (1 - self.tau) * target_param)
+
+        self.update_step += 1
